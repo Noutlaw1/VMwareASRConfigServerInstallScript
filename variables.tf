d8ee517422819bfdfecdad7463b6448628c44be2
Nick Outlaw <nickoutlaw@gmail.com>
2020-02-16 04:54:13 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "SUBID"  {
+  type = string
+}
+
+variable "CLIENTID" {
+  type = string
+}
+
+variable "CERTPATH" {
+  type= string
+}
+
+variable "CERTPASS" {
+  type = string
+}
+
+variable "TENANTID" {
+  type = string
+}
+
+variable "DEFAULTUSER" {
+  type = string
+}
+
+variable "DEFAULTPASSWORD" {
+  type = string
+}
+
+variable "location" {
+  type = "string"
+  default = "eastus"
+}
