5bbbff1c0b477c38edc91d6ce9ef75e0113d77a8
Nick Outlaw <nickoutlaw@gmail.com>
2020-03-06 18:52:58 -0500

    Update CSPSInstallScript.ps1

--- CSPSInstallScript.ps1
+++ CSPSInstallScript.ps1
@@ -26,7 +26,7 @@ while ($vds.Status -ne "Running")
     }
 $disk = Get-Disk | Where {$_.PartitionStyle -eq "RAW"} 
 $disk | Initialize-Disk -PartitionStyle MBR 
-$disk | New-Partition -DriveLetter "F" -UseMaximumSize Format-Volume -FileSystem NTFS -NewFileSystemLabel "ASR_Disk" -Confirm:$false
+$disk | New-Partition -DriveLetter "F" -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "ASR_Disk" -Confirm:$false
 #Put Windows update to Manual as it bogs down the system for smaller VMs in my experience.
 Write-Log("Setting Windows Update to manual.") 
 $Service = Get-Service -Name "Windows Update" 
